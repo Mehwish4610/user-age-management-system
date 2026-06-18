a463d90cbaf237746c64ef9cd7ec4c18f8df20f7
mehwish <mehwishafreen10303@gmail.com>
2026-06-18 19:20:10 +0530

    api url update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8081",
+  baseURL: import.meta.env.VITE_API_URL || "https://user-age-management-system.onrender.com",
 });
 
 export const getUsers = (page = 1, limit = 5) => {
